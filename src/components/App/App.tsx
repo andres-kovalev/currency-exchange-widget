e1d584873f5144c82ee6668b290e7191b0f25a76
Andres Kovalev <mail@akovalev.ru>
2019-12-29 21:05:54 +0300

    feat: customized components appearance

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { StylesProvider } from '@material-ui/core/styles';
 
 import configureStore from '../../store';
 import { Routes } from '..';
@@ -8,9 +9,11 @@ import pockets from '../../const/pockets.json';
 const store = configureStore({ pockets });
 
 const App: React.FC = () => (
-    <Provider store={ store }>
-        <Routes />
-    </Provider>
+    <StylesProvider injectFirst>
+        <Provider store={ store }>
+            <Routes />
+        </Provider>
+    </StylesProvider>
 );
 
 export default App;
